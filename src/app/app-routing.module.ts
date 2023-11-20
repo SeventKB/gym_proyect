a758e954c294a0b3a763cd17f24570ee79598509
joan andres vera plua <joanvera-13@hotmail.com>
2023-11-20 10:46:40 -0500

    Update rutinas multimedia, modulo tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { RutinasMultimediaComponent } from './rutinas-multimedia/rutinas-multimedia.component';
 
 const routes: Routes = [
   {path:'', component:DashboardComponent},
-  {path:'Pagina-Principal', component:PaginaPrincipalComponent}
+  {path:'Pagina-Principal', component:PaginaPrincipalComponent},
+  {path:'Rutina-Miltimedia', component:RutinasMultimediaComponent}
 ];
 
 @NgModule({
